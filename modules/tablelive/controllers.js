ee2cb95851283ba1b15a198482e7644969f3009f
nuchanadk <55680032+nuchanadk@users.noreply.github.com>
2020-11-05 14:18:54 +0700

    update tablelive 2020.11.5

--- modules/tablelive/controllers.js
+++ modules/tablelive/controllers.js
@@ -19,12 +19,35 @@ function ($scope,$http,$interval) {
 
         return [year, month, day].join('-');
     }
+
+        $scope.set_style = function (item) {
+            if (item == "HH") 
+            {
+                return { 'font-weight': 'bold',float: "right",color: "#FF0000" }
+            }
+            else if (item == "H") 
+            {
+                return { 'font-weight': 'bold',float: "right",color: "#ff9900" }
+            }
+            else if (item == "L") 
+            {
+                return { 'font-weight': 'bold',float: "right",color: "#e6e600" }
+            }
+            else if (item == "LL") 
+            {
+                return { 'font-weight': 'bold',float: "right",color: "#996600" }
+            }
+            else{ return { float: "right",color: "#000000" } }
+        }
         
         $scope.date1 = new Date();
         $scope.date2 = new Date();
-        $scope.show = true;
+        $scope.showspinner = true;
+        $scope.lastdata = [];
 
         $scope.datetitle = formatDate(new Date());
+
+        //var exists = 0;
 
         var loadData=function(){  
 
@@ -32,16 +55,54 @@ function ($scope,$http,$interval) {
                
                 //console.log(response.data);
                 if (response) { 
-                    $scope.show = false;
+                    $scope.showspinner = false;
                 } 
                 $scope.datatable = response.data;
+                
+                //console.log($scope.lastdata);
+                if($scope.lastdata.length === 0)
+                    $scope.lastdata = $scope.datatable;
 
+                for (var i=0; i< $scope.datatable.length; i++) {
+                
+                    //$scope.lastdata.push($scope.datatable[i]);
+                    //console.log($scope.lastdata[i].stationID + '___' +$scope.datatable[i].stationID);
+                    if ($scope.lastdata[i].stationID == $scope.datatable[i].stationID) {
+                        //$scope.lastdata[i].dataValue = $scope.datatable[i].dataValue;
+                        //console.log($scope.datatable[i].dataValue + '___' +$scope.lastdata[i].dataValue);
+                        if($scope.datatable[i].dataValue > $scope.lastdata[i].dataValue)
+                        {
+                            $scope.datatable[i].dataup = true ;
+                            $scope.datatable[i].datadown = false ;
+                        
+                        }
+                        else if($scope.datatable[i].dataValue < $scope.lastdata[i].dataValue)
+                        {
+                            $scope.datatable[i].dataup = false ;
+                            $scope.datatable[i].datadown = true ;
+                        }
+                        else
+                        {
+                            $scope.datatable[i].dataup = false ;
+                            $scope.datatable[i].datadown = false ;
+                        }
+                    }
+                    //console.log($scope.lastdata[i].dataValue);
+                    $scope.lastdata[i].dataValue = $scope.datatable[i].dataValue ;
+                    //console.log($scope.datatable[i].dataValue);
+                    //exists = 1;
+                }
+                
             }, function errorCallback(response) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
                 console.log("error"+' '+response);
             });
+
+            //console.log($scope.lastdata);
         }
+
+        //console.log($scope.lastdata);
 
         loadData();
         $interval(loadData, 60000);
--- modules/tablelive/selectdata.php
+++ modules/tablelive/selectdata.php
@@ -1,6 +1,7 @@
 <?php
 require_once ('../../database/db.php');
 require_once ('../../database/model/TTdata.php');
+require_once ('../../database/model/TMalarmconfig.php');
 $req = file_get_contents("php://input");
 $get = json_decode(stripslashes($req));
 header("Access-Control-Allow-Origin: * ");
@@ -15,9 +16,9 @@ $conn = $databaseService->getConnection();
 $items = new TTdata($conn);
 $levelArr = array();
 
-
- $stmt = $items->getSingleData();
- $itemCount = $stmt->rowCount();
+$stmt = $items->getSingleDataRealtime();
+$itemCount = $stmt->rowCount();
+//echo $itemCount;
 
 if($itemCount > 0){
         
@@ -25,14 +26,53 @@ if($itemCount > 0){
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 		extract($row);
 		//echo $row; 
+
+		$sqlQuery2 = " SELECT dataDatetime, dataValue FROM TTdata WHERE deviceID = '$deviceID' ORDER BY dataDatetime DESC LIMIT 1 ";
+		$stmt2 = $conn->prepare($sqlQuery2);
+		$stmt2->execute();
+		$dataRow = $stmt2->fetch(PDO::FETCH_ASSOC);
+		$dataDatetime = $dataRow['dataDatetime'];
+		$dataValue = $dataRow['dataValue'];
 		$date = strtotime($dataDatetime); 
 		//echo date('d/M/Y H:i:s', $date); 
+
+		$sqlQuery3 = " SELECT stationID, alarmLL, alarmL, alarmH, alarmHH FROM TMalarmconfig WHERE stationID = '$stationID' and alarmStatus = 1  ";
+		$stmt3 = $conn->prepare($sqlQuery3);
+		$stmt3->execute();
+		$dataalarm = $stmt3->fetch(PDO::FETCH_ASSOC);
+		$alarmLL = $dataalarm['alarmLL'];
+		$alarmL = $dataalarm['alarmL'];
+		$alarmH = $dataalarm['alarmH'];
+		$alarmHH = $dataalarm['alarmHH'];
+
+		if ( $dataValue < $alarmLL && $alarmLL != null )
+		{
+			$alarm_level = "LL";
+		}
+		else if ( $dataValue < $alarmL && $alarmL != null )
+		{
+			$alarm_level = "L";
+		}
+		else if ( $dataValue > $alarmHH && $alarmHH != null )
+		{
+			$alarm_level = "HH";
+		}
+		else if ( $dataValue > $alarmH && $alarmH != null )
+		{
+			$alarm_level = "H";
+		}
+		else
+		{
+			$alarm_level = "N";
+		}
+
 		$e = array(
 			"stationName" => $stationName,
 			"stationID" => $stationID,
 			"deviceID" => $deviceID,
 			"dataValue" => $dataValue,
-			"dataDatetime" => date('d-m-Y H:i', $date)	
+			"dataDatetime" => date('d-m-Y H:i', $date),
+			"alarmlevel" => $alarm_level
 		);
 
 		array_push($levelArr, $e);
